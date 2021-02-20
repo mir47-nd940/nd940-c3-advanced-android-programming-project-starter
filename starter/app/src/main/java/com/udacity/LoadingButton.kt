0c3ed8666c71c707bf5208a943efa37bdfa0d0ed
miro <minev.miro@gmail.com>
2021-02-20 17:09:41 +0200

    draw animated loading circle

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -2,9 +2,7 @@ package com.udacity
 
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
@@ -18,12 +16,14 @@ class LoadingButton @JvmOverloads constructor(
     private var _widthSize = 0
     private var _heightSize = 0
     private var _textSize = 60F
-    private var _text = context.getString(R.string.button_name)
-
+    private var _textInactive = context.getString(R.string.button_name)
+    private var _textActive = context.getString(R.string.button_loading)
+    private var _textDisplay = _textInactive
     private var _currentPercentage: Float = 0F
     private var _targetPercentage: Float = 0F
-
     private var _loadingAnimationListener: LoadingAnimationListener? = null
+    private var _textActiveWidth = 0
+    private lateinit var _loadingCircle: RectF
 
     fun setLoadingAnimationListener(listener: LoadingAnimationListener) {
         _loadingAnimationListener = listener
@@ -31,10 +31,10 @@ class LoadingButton @JvmOverloads constructor(
 
     private var _buttonState: ButtonState by Delegates.observable(ButtonState.Completed) { _, _, new ->
         when (new) {
-            ButtonState.Clicked -> _text = context.getString(R.string.button_loading)
-            ButtonState.Loading -> _text = context.getString(R.string.button_loading)
+            ButtonState.Clicked -> _textDisplay = _textInactive
+            ButtonState.Loading -> _textDisplay = _textActive
             ButtonState.Completed -> {
-                _text = context.getString(R.string.button_name)
+                _textDisplay = _textInactive
                 _currentPercentage = 0F
                 _targetPercentage = 0F
             }
@@ -44,6 +44,7 @@ class LoadingButton @JvmOverloads constructor(
 
     private val _drawColor = ResourcesCompat.getColor(resources, R.color.colorPrimary, null)
     private val _drawColorDark = ResourcesCompat.getColor(resources, R.color.colorPrimaryDark, null)
+    private val _drawColorAccent = ResourcesCompat.getColor(resources, R.color.colorAccent, null)
 
     private val _paint = Paint().apply {
         color = _drawColor
@@ -65,7 +66,17 @@ class LoadingButton @JvmOverloads constructor(
         strokeWidth = STROKE_WIDTH
     }
 
-    private val _paintWhite = Paint().apply {
+    private val _paintAccent = Paint().apply {
+        color = _drawColorAccent
+        isAntiAlias = true
+        isDither = true
+        style = Paint.Style.FILL
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = STROKE_WIDTH
+    }
+
+    private val _paintText = Paint().apply {
         color = Color.WHITE
         style = Paint.Style.FILL
         textSize = _textSize
@@ -86,10 +97,18 @@ class LoadingButton @JvmOverloads constructor(
         )
 
         canvas.drawText(
-            _text,
+            _textDisplay,
             _widthSize / 2F,
             (_heightSize + _textSize) / 2,
-            _paintWhite
+            _paintText
+        )
+
+        canvas.drawArc(
+            _loadingCircle,
+            0F,
+            _currentPercentage * 360 / 100,
+            true,
+            _paintAccent
         )
     }
 
@@ -103,6 +122,19 @@ class LoadingButton @JvmOverloads constructor(
         )
         _widthSize = w
         _heightSize = h
+
+        val bounds = Rect()
+        _paintText.getTextBounds(_textActive, 0, _textActive.length, bounds)
+        _textActiveWidth = bounds.width()
+        val offset = _textActiveWidth + ((_widthSize - _textActiveWidth) / 2F)
+        val quarterHeight = _heightSize / 4F
+        _loadingCircle = RectF(
+            offset,
+            quarterHeight,
+            offset + (quarterHeight * 2),
+            quarterHeight * 3
+        )
+
         setMeasuredDimension(w, h)
     }
 
@@ -115,7 +147,9 @@ class LoadingButton @JvmOverloads constructor(
     }
 
     fun updateProgress(percentage: Int) {
-        if (percentage <= 0) {
+        _buttonState = ButtonState.Loading
+
+        if (percentage.toFloat() == _targetPercentage) {
             _loadingAnimationListener?.onPartialAnimationComplete()
             return
         }
@@ -138,7 +172,6 @@ class LoadingButton @JvmOverloads constructor(
                 }
             }
             start()
-            _buttonState = ButtonState.Loading
         }
     }
 
